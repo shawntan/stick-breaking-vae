1f3ad1a8ba9f7b6255825d2ce68830afab7184f7
Shawn Tan <shawn@wtf.sg>
2016-11-08 14:45:44 -0500

    Modified stick_break_vae.py with comments

--- stick_break_vae.py
+++ stick_break_vae.py
@@ -1,41 +1,27 @@
 import theano.tensor as T
 from theano_toolkit import utils as U
-from theano_toolkit import hinton
 
-# Beta sampling via inverse CDF
+
 def beta_sample(alpha):
+    # Beta sampling via inverse CDF
     u = U.theano_rng.uniform(size=alpha.shape)
     return 1 - u ** (1 / alpha)
 
-# KL-divergence between gaussians
-def gauss_kl_divergence(mu, sigma_sq):
-    # Divergence from standard gaussian
-    k = mu.shape
-    return 1/2*(T.nlinalg.trace(sigma_sq) + T.dot(mu, mu - k + T.log(T.det(sigma_sq))))
 
-                                                 
 def beta_kl_divergence(alpha):
     # Divergence from Beta(1, 1)
     b = alpha
     return T.log(b) + (1 - b) / b
 
-# KL-divergence between betas (to be completed...)
-def betas_kl_divergence(alpha1, alpha2, T):
-    # Divergence between beta(1, alpha1) and beta(1, alpha2)
-    # T is truncation of the power series
-    pass
-        
-# gaussian part of the full kl_divergence
-def gauss_kl_divergence_sum(mus, sigma_sqs):
-    return T.sum(gauss_kl_divergence(mus, sigma_sqs), axis-=1)
 
-# beta part of the full kl-divergence     
 def kl_divergence(alphas):
+    # beta part of the full kl-divergence
     return T.sum(beta_kl_divergence(alphas),
                  axis=-1)
 
-# strick breaking construction
+
 def build_sample_pi(size=20):
+    # strick breaking construction
     idx = T.arange(size)
     cum_mat = (idx.dimshuffle(0, 'x') <
                idx.dimshuffle('x', 0))
@@ -48,4 +34,3 @@ def build_sample_pi(size=20):
         log_probs = T.dot(neg_log_v, cum_mat) + log_v
         return log_probs
     return sample_log_pi
-
